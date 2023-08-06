36cd56048e091567bdb0cfca4b6bac205910c832
Sam <robinson.will.sam@gmail.com>
2023-08-06 01:47:26 -0500

    strings

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # w3-java
+
+## String Methods:
+### https://www.w3schools.com/java/java_ref_string.asp
---
+++ Strings.java
@@ -0,0 +1,19 @@
+public class Strings {
+    public static void main(String[] args){
+
+        // String greeting = "Hello";
+
+        // String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        // System.out.println("The length of the txt string is: " + txt.length()); 
+
+        // String txt = "Hello World";
+        // System.out.println(txt.toUpperCase());
+        // System.out.println(txt.toLowerCase());
+
+        String txt = "Please locate where 'locate' occurs!";
+        System.out.println(txt.indexOf("locate")); // Outputs 7
+
+
+
+    }
+}
