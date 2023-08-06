4aea8adb71fe06a7e14890909539822d8aa2acf9
Sam <robinson.will.sam@gmail.com>
2023-08-06 01:09:13 -0500

    first commit

---
+++ Main.java
@@ -0,0 +1,49 @@
+//https://www.w3schools.com/java/java_syntax.asp
+public class Main {
+    public static void main(String[] args){
+        
+        // System.out.println("Hello World");
+
+        // int myNum = 16;
+        // System.out.println(myNum);
+        
+
+
+        // int myNum;
+        // myNum = 15;
+        // System.out.println(myNum);
+
+        // String name = "Sam";
+        // System.out.println("Hello " + name);
+
+        // String firstName = "Sam";
+        // String lastName = "Doe";
+        // String fullName = firstName + " " + lastName;
+        // System.out.println(fullName);
+
+        // int x = 5;
+        // int y = 6;
+        // System.out.println(x+y);
+
+        // int x = 5;
+        // int y = 6; 
+        // int z = 50;
+        // System.out.println(x + y + z);
+
+        // int x = 5, y = 6, z = 50;
+        // System.out.println(x + y + z);
+
+        // int x, y, z;
+        // x = y = z = 50;
+        // System.out.println(x + y + z);
+
+        // byte myNum = 100;
+        // System.out.println(myNum);
+
+
+
+
+        
+    }
+}
+
