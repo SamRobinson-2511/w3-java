9718a7c145ab8f24ae1d4a5d0a8510940e50ca37
Sam <robinson.will.sam@gmail.com>
2023-08-06 01:39:55 -0500

    operators

---
+++ Typecasting.java
@@ -0,0 +1,21 @@
+public class Typecasting {
+    public static void main(String[] args){
+        
+    //widening casting
+    // int myInt = 9;
+    // double myDouble = myInt;
+
+    // System.out.println(myInt);
+    // System.out.println(myDouble);
+
+    double myDouble = 9.78d;
+    int myInt = (int) myDouble;
+
+    System.out.println(myDouble);
+    System.out.println(myInt);;
+
+
+    
+
+    }
+}
